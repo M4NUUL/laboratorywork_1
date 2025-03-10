e6aa108fff46ae4e45cbb30e126940c12af8fd50
m4nuul <nikitakortrlr020@gmail.com>
2025-03-10 11:01:41 +0700

    добавлена проверка на корректность данных

--- geometry.cpp
+++ geometry.cpp
@@ -3,20 +3,31 @@
 
 using namespace std;
 
-void Circle(){
+int Circle(){
     double pi = M_PI;
     cout << "ВВедите радиус окружности: ";
     int radius;
     cin >> radius;
+    if (radius < 0)
+    {
+        cout << "некоректный радиус";
+        return 0;
+    }
     cout << "Введите угол сектора (в градусах): ";
     int alpha;
     cin >> alpha;
+    if (alpha < 0 || alpha > 360)
+    {
+        cout << "Угол больше 360 градусов или меньше 0";
+        return 0;
+    }
     double length_circle = 2 * pi * radius;
     double square_circle = pi * radius * radius;
     double square_sector = square_circle * alpha / 360;
     cout << "Длина окружности: " << length_circle << endl;
     cout << "Площадь окружности: " << square_circle << endl;
     cout << "Площадь сектора: " << square_sector << endl;
+    return 0;
 }
 
 int main(){
